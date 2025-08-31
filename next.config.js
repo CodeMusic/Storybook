4377f0dd4df24b8c2f2c2249228fe45a848f5f2f
CodeMusic <themusicofthecode@gmail.com>
2025-08-31 09:15:43 -0400

    story compile

--- next.config.js
+++ next.config.js
@@ -1,13 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: '**' }
-    ]
-  }
-};
-
-module.exports = nextConfig;
-
-
+    reactStrictMode: true,
+  
+    // Enable static export for Apache
+    output: 'export',
+  
+    // Needed if you use <Image/> with export
+    images: {
+      unoptimized: true,
+      remotePatterns: [
+        { protocol: 'https', hostname: '**' }
+      ]
+    },
+  
+    // Optional but helps with static hosting
+    trailingSlash: true
+  };
+  
+  module.exports = nextConfig;
